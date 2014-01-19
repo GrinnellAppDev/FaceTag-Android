f17450e293adf1ce61d45582ce524cc67dcc6326
triestpa <triestpa@grinnell.edu>
2014-01-19 00:31:09 -0500

    add camera button to game detai view

--- src/com/facetag/android/GameInfoFragment.java
+++ src/com/facetag/android/GameInfoFragment.java
@@ -1,27 +1,24 @@
 package com.facetag.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.facetag.android.parse.Game;
 import com.facetag_android.R;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 public class GameInfoFragment extends Fragment {
 	ParseUser mUser = ParseUser.getCurrentUser();
 	ParseUser mTarget;
 	final String TAG = "Game Info Screen";
+	GameScreenActivity mActivity;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,21 +26,37 @@ public class GameInfoFragment extends Fragment {
 		View v = inflater
 				.inflate(R.layout.fragment_game_info, container, false);
 
-		GameScreenActivity mActivity = (GameScreenActivity) getActivity();
+		mActivity = (GameScreenActivity) getActivity();
 
-		TextView targetInfo = (TextView) v.findViewById(R.id.target_description);
+		TextView targetInfo = (TextView) v
+				.findViewById(R.id.target_description);
 		ImageView targetPic = (ImageView) v.findViewById(R.id.target_photo);
 		TextView gameName = (TextView) v.findViewById(R.id.game_name);
 
 		ImageLoader.getInstance().displayImage(
 				mUser.getString("profilePictureURL"), targetPic);
-		
+
 		gameName.setText(mActivity.mGame.getName());
-		
+
+		Button cameraButton = (Button) v.findViewById(R.id.camera_button);
+
+		cameraButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				launchCamera();
+			}
+		});
 		return v;
 	}
 
 	public void judgePhotos() {
 
+	}
+
+	public void launchCamera() {
+		Intent intent = new Intent(mActivity, CameraActivity.class);
+		intent.putExtra("Game Name", mActivity.mGame.getName());
+		startActivity(intent);
 	}
 }
