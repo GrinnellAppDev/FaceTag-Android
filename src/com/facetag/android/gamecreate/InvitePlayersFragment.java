5d200e80eecd9a45caa67d4f51d1376eabf95c8c
triestpa <triestpa@grinnell.edu>
2014-02-16 14:07:42 -0600

    display names correctly in invite list

--- src/com/facetag/android/gamecreate/InvitePlayersFragment.java
+++ src/com/facetag/android/gamecreate/InvitePlayersFragment.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.MenuItem;
 import com.facetag_android.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -25,6 +26,7 @@ public class InvitePlayersFragment extends SherlockFragment {
 	CreateGameActivity mActivity;
 	ArrayList<ParseUser> mUsers = new ArrayList<ParseUser>();
 	ListView mListView;
+	View fragView;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,18 +34,6 @@ public class InvitePlayersFragment extends SherlockFragment {
 
 		mActivity = (CreateGameActivity) getSherlockActivity();
 		setHasOptionsMenu(true);
-		
-		//get players on parse
-		ParseQuery<ParseUser> query = ParseUser.getQuery();
-		query.findInBackground(new FindCallback<ParseUser>() {
-		  public void done(List<ParseUser> users, ParseException e) {
-		    if (e == null) {
-		    	mUsers.addAll(users);
-		    } else {
-		    	Log.i(TAG, e.toString());
-		    }
-		  }
-		});
 	}
 
 	@Override
@@ -51,14 +41,24 @@ public class InvitePlayersFragment extends SherlockFragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_invite_list, container,
 				false);
-
 		mActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-		ArrayAdapter<ParseUser> inviteAdapter = new ScoreListAdapter(mActivity,
-				R.layout.invite_list_adapter, mUsers);
 		mListView = (ListView) v.findViewById(R.id.invitelist);
-		mListView.setAdapter(inviteAdapter);
-
+		
+		//get players on parse
+		ParseQuery<ParseUser> query = ParseUser.getQuery();
+		query.findInBackground(new FindCallback<ParseUser>() {
+		  public void done(List<ParseUser> users, ParseException e) {
+		    if (e == null) {
+		    	Log.i(TAG, users.size() + " Users Retrieved");
+		    	mUsers.addAll(users);
+		    	ArrayAdapter<ParseUser> inviteAdapter = new ScoreListAdapter(mActivity,
+						R.layout.invite_list_adapter, mUsers);
+				mListView.setAdapter(inviteAdapter);
+		    } else {
+		    	Log.e(TAG, e.toString());
+		    }
+		  }
+		});
 		return v;
 	}
 
@@ -82,9 +82,26 @@ public class InvitePlayersFragment extends SherlockFragment {
 			View rowView = inflater.inflate(layoutResourceId, parent, false);
 
 			TextView nameText = (TextView) rowView.findViewById(R.id.invitee);
-			nameText.setText(users.get(position).getUsername());
+		//	nameText.setText(users.get(position).getUsername());
+			nameText.setText(users.get(position).getString("fullName"));
+
 			
 			return rowView;
 		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			GameSettingsFragment settingsFrag = new GameSettingsFragment();
+			mActivity.getSupportFragmentManager().beginTransaction()
+					.replace(R.id.create_fragment_container, settingsFrag).commit();
+			return true;
+		default:
+			super.onOptionsItemSelected(item);
+		}
+		return true;
 	}
 }
